c4184d0edb028cd32aed1b6aca823c4bde32d055
Zaida88 <zsm.mejia@yavirac.edu.ec>
2023-01-28 12:56:20 -0500

    proyect

--- views/admin/dashboard-admin.php
+++ views/admin/dashboard-admin.php
@@ -1,1 +1,25 @@
-<h1>tablero de admin</h1>
+<div class="content">
+  <h1 class="d-flex justify-content-center"><b>Lenguajes</b></h1><br>
+  <div class="row">
+    <?php
+    $item = null;
+    $value = null;
+    $languages = DashboardClientController::ctrShowLanguages($item, $value);
+    foreach ($languages as $key => $language) {
+      echo '<div class="col-sm-4 mb-5 mb-sm-0 languages">
+      <a idLanguage="' . $language["id"] . '" btnShowLanguage  href="' . $language["route"] . '" style="text-decoration: none; color: black;" >
+        <div class="card">
+        <img src="' . $language["logo"] . '" class="card-img-top" alt="' . $language["name"] . '" style="width:100%;height: 245px;">
+        <div class="card-body">
+        <div class="d-flex justify-content-center">
+        <h5 class="card-title" style="margin-bottom: 0;">' . $language["name"] . '</h5>
+        </div>
+        <p class="card-text">' . $language["description"] . '</p><br>
+        </div>
+        </div>
+        </a>
+        </div>';
+    }
+    ?>
+  </div>
+</div>
--- views/admin/proyect.php
+++ views/admin/proyect.php
@@ -1,45 +1,183 @@
-    <link rel="stylesheet" href="assets/css/proyect.css">
-    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
-<br><br>
+<link rel="stylesheet" href="assets/css/proyect.css">
 
-<div class="container-every">
+
+<div class="content">
   <div class="header">
-			<ul class="nav">
-				<li><a href="">Informacion Pagina</a></li>
-				<li><a href="">Lenguajes</a></li>
-			</ul>
+    <ul class="nav">
+      <li><a href="proyect"><b>Informacion Pagina</b></a></li>
+      <li><a href="lenguajes"><b>Lenguajes</b></a></li>
+    </ul>
   </div>
 
-<div class="container">
-  <table class="table table-sm table-striped">
-    <thead class="table-dark">
-      <tr>
-        <th>Nombre</th>
-        <th>Descripcion</th>
-        <th>logo</th>
-        <th>Correo</th>
-        <th>Numero de telefono</th>
-      </tr>
-    </thead>
- <tbody>
-    
-  </tbody> 
+  <div class="box">
 
-  <?php foreach ($data["proyect"] as $dato){
+    <div class="box-body">
 
-    echo "<tr>";
-    echo "<td>".$dato["name"]."</td>";
-    echo "<td>".$dato["description"]."</td>";
-    echo "<td>".$dato["logo"]."</td>";
-    echo "<td>".$dato["email"]."</td>";
-    echo "<td>".$dato["phone_number"]."</td>";
-    echo "</tr>";
-  }
-  ?>
-  </table>
+      <table class="table table-bordered table-striped dt-responsive tablas">
 
-</div>
-</div>
+        <thead style="background-color:white; color:black; border: 1px white solid;">
+          <tr>
+            <th style="width:15%;">Nombre</th>
+            <th style="width:25%;">Descripcion</th>
+            <th style="">Logo</th>
+            <th style="width:20%;">Correo</th>
+            <th style="width:20%;">Telefono</th>
+          </tr>
+        </thead>
 
-</body>
-</html>
+        <tbody>
+          <?php
+      $item = null;
+      $valor = null;
+      $proyect = ProyectController::ctrShowProyect($item, $valor);
+
+      foreach ($proyect as $key => $value) {
+        echo '<td>' . $value["name"] . '</td>
+              <td>' . $value["description"] . '</td>
+              <td><img src="' . $value["logo"] . '" width="150px"></td>
+              <td>' . $value["email"] . '</td>
+              <td>' . $value["phone_number"] . '</td>
+              
+              <td>
+              <div class="btn-group" >
+              <button class="btn btn-warning btnUpdateProyect" idProyect="'.$value["id"].'" data-toggle="modal" data-target="#modalUpdateProyect"><i class="fa fa-pencil"></i></button>
+              </div>
+              </td>
+            </tr>';
+      }
+      ?>
+        </tbody>
+
+      </table>
+
+    </div>
+  </div>
+
+  <!--=====================================
+MODAL EDITAR PROYECTO
+======================================-->
+
+  <div id="modalUpdateProyect" class="modal fade" role="dialog">
+
+    <div class="modal-dialog">
+
+      <div class="modal-content">
+
+        <form role="form" method="post" enctype="multipart/form-data">
+
+          <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+          <div class="modal-header" style="background:#04246E; color:white">
+
+            <button type="button" class="close" data-dismiss="modal"
+              style="background:#04246E; color:white">&times;</button>
+
+            <h4 class="modal-title" style="background:#04246E;">Editar Proyecto</h4>
+
+          </div>
+
+          <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+          <div class="modal-body" style="background:white; color:white">
+
+            <div class="box-body" style="background:white; color:white; border: 2px white solid;">
+
+              <!-- ENTRADA PARA EL NOMBRE -->
+
+              <div class="form-group">
+
+                <div class="input-group">
+
+                  <span class="input-group-addon" style="background:white; color:#04246E; border: 2px #04246E solid;"><i
+                      class="fa fa-user"></i></span>
+
+                  <input type="text" class="form-control input-lg" id="updateName" name="updateName" value=""
+                    style="border: 2px #04246E solid;background:#04246E;color:white;" required>
+
+                </div>
+
+              </div>
+
+              <!-- ENTRADA PARA EL PROYECT -->
+
+              <div class="form-group">
+
+                <div class="input-group">
+
+                  <span class="input-group-addon" style="background:white; color:#04246E; border: 2px #04246E solid;"><i
+                      class="fa fa-key"></i></span>
+
+                  <input type="text" class="form-control input-lg" id="updateProyect" name="updateProyect" value=""
+                    style="border: 2px #04246E solid;background:white;color:#04246E;" readonly>
+
+                </div>
+
+              </div>
+
+              <!-- ENTRADA PARA SUBIR FOTO -->
+
+              <div class="form-group">
+
+                <div class="panel" style="background:white; color:#04246E; border: 2px white solid;">SUBIR FOTO</div>
+
+                <input type="file" class="nuevologo" name="updateLogo"
+                  style="background:#04246E; color:white; border: 2px #04246E solid;">
+
+                <p class="help-block">Peso máximo de la foto 2MB</p>
+
+                <img src="assets/img/proyect/default/anonymous.png"
+                  style="background:#04246E; color:#04246E; border: 2px #04246E solid;"
+                  class="img-thumbnail previsualizarEditar" width="130px">
+
+                <input type="hidden" name="logoActual" id="logoActual">
+
+              </div>
+
+            </div>
+
+          </div>
+
+          <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+
+          <div class="modal-footer" style="background:white; color:white; border: 2px white solid;">
+
+            <button type="button" class="btn btn-default pull-left" data-dismiss="modal"
+              style="background:#6A0436; color:white; border: 2px #6A0436 solid;">Salir</button>
+
+            <button type="submit" class="btn btn-primary"
+              style="background:#04246E; color:white; border: 2px #04246E solid;">Modificar Proyecto</button>
+
+          </div>
+
+          <?php
+
+          $updateProyect = new ProyectController();
+          $updateProyect -> ctrUpdateProyect();
+
+        ?>
+
+        </form>
+
+      </div>
+
+    </div>
+
+  </div>
+
+
+
+
+
+
+
+
+
+  </body>
+
+  </html>
