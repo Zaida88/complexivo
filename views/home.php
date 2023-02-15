46ed21c577fc28de30d77ba642198756e14b2b92
Liseth Ponce <lka.ponce@yavirac.edu.ec>
2023-02-14 21:38:36 -0500

    cambios en los estilos: home

--- views/home.php
+++ views/home.php
@@ -2,32 +2,98 @@
 $item = null;
 $value = null;
 $project = ProjectController::ctrShowProject($item, $value);
-foreach ($project as $key => $value) {
-    echo '
-    <div class="container d-flex justify-content-center">
-    <div class="col-lg-9 row p-4 m-4 pt-lg-4 pb-lg-2 rounded-5 align-items-center shadow-lg bg-dark">
-      <div class="col-lg-7 p-3 p-lg-5 pt-lg-1">
-        <h1 class="display-4 fw-bold lh-1 text-white">' . $value["name_project"] . '</h1>
-        <p class="lead text-white">'. $value["description_project"] . '</p>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
-        <a href="login"><button type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">Iniciar sesión</button></a>
-        <a href="registration"><button type="button" class="btn btn-light btn-lg px-4">Registrarse</button></a>
+foreach ($project as $key => $value) { ?>
+
+  <body class="d-flex flex-column h-100">
+    <main class="flex-shrink-0">
+      <header class="bg-dark py-5">
+        <div class="container px-5">
+          <div class="row gx-5 align-items-center justify-content-center">
+            <div class="col-lg-8 col-xl-7 col-xxl-6">
+              <div class="my-5 text-center text-xl-start">
+                <h1 class="display-5 fw-bolder text-white mb-2">
+                  <?php echo $value["name_project"] ?>
+                </h1>
+                <p class="lead fw-normal text-white-50 mb-4">
+                  <?php echo $value["description_project"] ?>
+                </p>
+                <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                  <a href="login"><button type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">Iniciar
+                      sesión</button></a>
+                  <a href="registration"><button type="button" class="btn btn-light btn-lg px-4">Registrarse</button></a>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center"><img class="img-fluid rounded-3 my-4"
+                src="<?php echo $value["logo_project"] ?>" alt="LOGO" /></div>
+          </div>
+        </div>
+      </header>
+      <section class="py-5">
+        <div class="container px-5 my-5">
+          <div class="row gx-5 justify-content-center">
+            <div class="col-lg-8 col-xl-6">
+              <div class="text-center">
+                <h2 class="fw-bolder">Contenido</h2>
+                <p class="lead fw-normal text-muted mb-5">Lenguajes disponiples actualmente</p>
+              </div>
+            </div>
+          </div>
+          <div class="row gx-5">
+
+            <div class="col-lg-4 mb-5">
+              <div class="card h-100 shadow border-0">
+                <img class="card-img-top" src="assets/img/home/html.jpg" alt="html" />
+                <div class="card-body p-4">
+                  <a class="text-decoration-none link-dark stretched-link" href="#!">
+                    <h5 class="card-title mb-3">HTML</h5>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 mb-5">
+              <div class="card h-100 shadow border-0">
+                <img class="card-img-top" src="assets/img/home/js.png" alt="js" />
+                <div class="card-body p-4">
+                  <a class="text-decoration-none link-dark stretched-link" href="#!">
+                    <h5 class="card-title mb-3">JavaScript</h5>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 mb-5">
+              <div class="card h-100 shadow border-0">
+                <img class="card-img-top" src="assets/img/home/css.jpg" alt="css" />
+                <div class="card-body p-4">
+                  <a class="text-decoration-none link-dark stretched-link" href="#!">
+                    <h5 class="card-title mb-3">CSS</h5>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </section>
+    </main>
+    <footer class="bg-dark py-4 mt-auto">
+      <div class="container px-5">
+        <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+          <div class="col-auto">
+            <div class="small m-0 text-white">Copyright &copy; Worldcodes 2023</div>
+          </div>
+          <div class="col-auto">
+            <li class="link-light small"><i class="fa-solid fa-phone">&nbsp;&nbsp;</i>
+              <?php echo $value["phone_number_project"] ?>
+            </li>
+            <li class="link-light small"><i class="fa-solid fa-envelope">&nbsp;&nbsp;</i>
+              <?php echo $value["email_project"] ?>
+            </li>
+          </div>
         </div>
       </div>
-      <div class="col-lg-4 offset-lg-1 p-0 shadow-lg">
-          <img src="' . $value["logo_project"] . '" alt="' . $value["logo_project"] . '" width="100%">
-      </div>
-    </div>
-  </div>
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-
-<ul class="nav col-md-4 justify-content-end">
-<h5 style="margin-right: 30%;">Información de contacto</h5>
-  <li class="nav-item" style="margin-right: 30%;"><i class="fa-solid fa-phone">&nbsp;</i>' . $value["phone_number_project"] . '</li>
-  <li class="nav-item" style="margin-right: 30%;"><i class="fa-solid fa-envelope">&nbsp;</i>' . $value["email_project"] . '</li>
-</ul><br><br>
-<p class="col-md-3 mb-0 text-muted">&copy; 2023 Worldcodes</p>
-</footer>
-';
+    </footer>
+  </body>
+  <?php
 }
 ?>
