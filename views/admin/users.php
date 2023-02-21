20591b4d97eff68ca8d8a733f8e3f3fa1cc97b4e
unknown <zsm.mejia@yavirac.edu.ec>
2023-02-21 18:03:15 -0500

    css-user

--- views/admin/users.php
+++ views/admin/users.php
@@ -86,66 +86,65 @@
   MODAL AGREGAR USUARIO
   ======================================-->
   <div class="modal fade" id="createUserModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
       <div class="modal-content">
           <div class="modal-header">
             <h1 class="modal-title fs-5" id="exampleModalLabel"><b>Nuevo Usuario</b></h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
         <div class="modal-body">
-          <form method="post" class="card rounded-5" enctype="multipart/form-data">
-                <div class="card-body">
-                    <?php
-                    $registration = new UsersController();
-                    $registration->ctrMakeUser();
-                    ?>
-                    <div class="row align-items-start">
-                        <div class="col">
-                            <div class="mb-3">
-                                <label for="newUsername" class="form-label">Nombre de usuario</label>
-                                <input type="text" name="newUsername" class="form-control" required>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="first_name" class="form-label">Nombre</label>
-                                <input type="text" name="first_name" class="form-control" required>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="last_name" class="form-label">Apellido</label>
-                                <input type="text" name="last_name" class="form-control" required>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Correo electrónico</label>
-                                <input type="email" name="email" class="form-control" required>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="mb-3">
-                                <label for="password1" class="form-label">Contraseña</label>
-                                <input type="password" name="password1" class="form-control" required>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="password2" class="form-label">Repetir contraseña</label>
-                                <input type="password" name="password2" class="form-control" required>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="newPhoto" class="form-label">Foto de perfil: </label>
-                                <input type="file" class="newPhoto" name="newPhoto" accept="image/*">
-                                <img src="assets/img/users/user-default.png" class="previewImg"
-                                    width="180px">
-                            </div>
-                        </div>
+          <form method="post" enctype="multipart/form-data">
+            <div class="card-body">
+              <?php
+                $registration = new UsersController();
+                $registration->ctrMakeUser();
+              ?>
+                <div class="row ">
+                  <div id="principal">
+                    <div class="mb-3">
+                      <label for="newUsername" class="form-label"><b>Nombre de usuario:</b></label>
+                      <input type="text" name="newUsername" class="form-control" required>
                     </div>
-                    <div class="float-sm-start ms-5">
-                        <a href="home"><button type="button" class="btn btn-secondary">Cancelar</button></a>
+
+                    <div class="mb-3">
+                      <label for="first_name" class="form-label"><b>Nombre:</b></label>
+                      <input type="text" name="first_name" class="form-control" required>
                     </div>
-              <div class="float-sm-end me-5">
-                  <button type="submit" class="btn btn-primary">Crear</button>
-              </div>
+
+                    <div class="mb-3">
+                      <label for="last_name" class="form-label"><b>Apellido:</b></label>
+                      <input type="text" name="last_name" class="form-control" required>
+                    </div>
+                  </div>
+                  <div id="sidebar">
+                    <div class="mb-3">
+                      <label for="email" class="form-label"><b>Correo electrónico:</b></label>
+                      <input type="email" name="email" class="form-control" required>
+                    </div>
+                    <div class="mb-3">
+                      <label for="password1" class="form-label"><b>Contraseña:</b></label>
+                      <input type="password" name="password1" class="form-control" required>
+                    </div>
+
+                    <div class="mb-3">
+                      <label for="password2" class="form-label"><b>Repetir contraseña:</b></label>
+                      <input type="password" name="password2" class="form-control" required>
+                    </div>
+                  </div>
+
+                    <div class="mb-3">
+                      <label for="newPhoto" class="form-label"><b>Foto de perfil:</b></label>
+                      <input type="file" class="newPhoto" name="newPhoto" accept="image/*">
+                      <img src="assets/img/users/user-default.png" class="previewImg"
+                        width="180px">
+                    </div>
+                </div>
+                <div class="float-sm-start ms-5">
+                  <a href="users"><button type="button" class="btn btn-secondary">Cancelar</button></a>
+                </div>
+                <div class="float-sm-end me-5">
+                    <button type="submit" class="btn btn-primary">Crear</button>
+                </div>
             </div>
           </form>
         </div>
