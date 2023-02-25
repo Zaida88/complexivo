82d4bd50db4e31b43578a41c27e866fc4433601b
Liseth Ponce <lka.ponce@yavirac.edu.ec>
2023-02-24 20:05:13 -0500

    opcion de agregar una nueva etiqueta

--- controllers/label.controller.php
+++ controllers/label.controller.php
@@ -2,6 +2,43 @@
 
 class LabelController
 {
+    static public function ctrCreateLabel()
+    {
+        if (isset($_POST['createLabel'])) {
+            $idLanguage = $_POST["idLanguage"];
+            $table1 = "labels";
+            $item1 = "name_label";
+            $value1 = $_POST["name_label"];
+            $option1 = "*";
+            $result1 = LabelModel::mdlLabelsAdminCreate($table1, $item1, $value1, $option1);
+
+            if (empty($result1)) {
+
+                $table = "labels";
+                $data = array(
+                    "idLanguage" => $_POST["idLanguage"],
+                    "name_label" => $_POST["name_label"],
+                    "description_label" => $_POST["description_label"]
+                );
+                $result = LabelModel::mdlCreateLabel($table, $data);
+
+                if ($result == "ok") {
+                    echo '<script>
+                    swal("Etiqueta agregado con exito", "", "success")
+                    .then((value) => {
+                        window.location = "index.php?route=list-labels&idLanguage=" + ' . $idLanguage . ';
+                    });
+                         </script>';
+                }   
+
+            } else {
+                echo '<script>
+                swal("El nombre de la etiqueta ya se encuentra registrado", "", "error");
+                     </script>';
+            }
+
+        }
+    }
     static public function ctrShowLabel($item, $value)
     {
         $table = "labels";
--- models/label.model.php
+++ models/label.model.php
@@ -4,6 +4,24 @@ require_once "db.php";
 
 class LabelModel
 {
+    static public function mdlCreateLabel($table, $data)
+    {
+        $stmt = Connect::connection()->prepare("INSERT INTO $table (idLanguage,name_label,description_label) VALUES (:idLanguage,:name_label,:description_label)");
+        $stmt->bindParam(":idLanguage", $data["idLanguage"], PDO::PARAM_INT);
+        $stmt->bindParam(":name_label", $data["name_label"], PDO::PARAM_STR);
+        $stmt->bindParam(":description_label", $data["description_label"], PDO::PARAM_STR);
+
+        if ($stmt->execute()) {
+
+            return "ok";
+
+        } else {
+
+            return "error";
+
+        }
+
+    }
     static public function mdlShowLabel($table, $item, $value)
     {
         $stmt = Connect::connection()->prepare("SELECT * FROM $table WHERE $item = :$item");
@@ -41,6 +59,19 @@ class LabelModel
         $stmt->execute();
         return $stmt->fetchAll();
 
+    }
+
+    static public function mdlLabelsAdminCreate($table, $item, $value, $optionEx)
+    {
+        $stmt = Connect::connection()->prepare("SELECT $optionEx FROM $table WHERE $item = :$item");
+        $stmt->bindParam(":" . $item, $value, PDO::PARAM_STR);
+        $stmt->execute();
+
+        if (isset($stmt)) {
+            return $stmt->fetchAll();
+        } else {
+            return null;
+        }
     }
 
 }
