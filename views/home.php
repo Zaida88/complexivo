646806396162cc4e9e464770aabdeb1f96919350
Zaida88 <zsm.mejia@yavirac.edu.ec>
2023-03-05 10:30:28 -0500

    css

--- views/home.php
+++ views/home.php
@@ -12,9 +12,9 @@ foreach ($project as $key => $value) { ?>
           <div class="row gx-5 align-items-center justify-content-center">
             <div class="col-lg-8 col-xl-7 col-xxl-6">
               <div class="my-5 text-center text-xl-start">
-                <h1 class="display-5 fw-bolder text-white mb-2">
+                <h3 class="display-5 fw-bolder text-white mb-2">
                   <?php echo $value["name_project"] ?>
-                </h1>
+                </h3>
                 <p class="lead fw-normal text-white text-opacity-75 mb-4">
                   <?php echo $value["description_project"] ?>
                 </p>
