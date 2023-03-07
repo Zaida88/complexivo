175729cea070304627a45cdb3be50ec50392b496
Liseth Ponce <lka.ponce@yavirac.edu.ec>
2023-03-07 15:19:43 -0500

    cambio en controlador user

--- controllers/user.controller.php
+++ controllers/user.controller.php
@@ -255,15 +255,6 @@ class UsersController
 										);
 
 										$reply = UsersModel::mdlCreateUser($table, $data);
-										function write_to_console($data) {
-											$console = $data;
-											if (is_array($console))
-											$console = implode(',', $console);
-										   
-											echo "<script>console.log('Console: " . $console . "' );</script>";
-										   }
-										   write_to_console($reply);
-										   write_to_console($data);
 
 										if ($reply == "ok") {
 											echo '<script>
