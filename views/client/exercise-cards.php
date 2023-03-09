787798e045ee8768e3003faa5ef1f4b03bea19be
Zaida88 <zsm.mejia@yavirac.edu.ec>
2023-03-09 16:02:58 -0500

    css

--- views/client/exercise-cards.php
+++ views/client/exercise-cards.php
@@ -3,12 +3,14 @@
   <div class="row text-white text-center">
     <div style="background-color:black;" class="col-sm-3">
       <div class="justify-content-center">
-        <div class="d-flex justify-content-start go mt-2">
-          <button type=" button" class="btn btn-dark back" idLabel="<?php echo $_GET["idLabel"]; ?>"
-            idLanguage="<?php echo $_GET["idLanguage"]; ?>">Regresar</button>
+        <div class="d-flex justify-content-start go mt-4">
+          <button style="background-color:#B61717; color:white;" type=" button" class="btn back" idLabel="<?php echo $_GET["idLabel"]; ?>"
+            idLanguage="<?php echo $_GET["idLanguage"]; ?>">
+            <i class="fa-sharp fa-solid fa-arrow-left"></i>&nbsp;Regresar
+          </button>
         </div>
         <div class="d-flex justify-content-center">
-          <h2 class="card-title text-dark mt-2"><b>Realizar ejercicio</b></h2>
+          <h2 style="color:#F4ED10;" class="card-title mt-4"><b>Realizar ejercicio</b></h2>
         </div>
         <div class="content d-flex justify-content-center">
           <?php
@@ -18,18 +20,20 @@
           $valueEx = $_GET["idLabel"];
           $valueEx2 = $_GET["numberExercise"];
           $result1 = ExerciseModel::mdlExercise($table, $itemEx, $valueEx, $itemEx2, $valueEx2); ?>
-          <div class="card border border-secondary rounded-4" style="width: auto;">
-            <div class="card-body bg-body-secondary rounded-4">
-              <h4 class="card-title d-flex justify-content-center text-dark" style="margin-bottom: -15px;">
+          <div style="background-color:black;"  class="card rounded-4" style="width: auto;">
+            <div style="background-color:black;" class="card-body rounded-4">
+              <h4 style="color:#10D4F4;" class="card-title d-flex justify-content-center" style="margin-bottom: -15px;">
                 <?php echo $result1["name_exercise"]; ?>
               </h4><br>
-              <p class="card-text text-dark">
+              <p class="card-text text-white">
                 <?php echo $result1["description_exercise"]; ?>
               </p>
-              <p class="d-flex justify-content-start text-dark"><b>Ejemplo:</b></p>
+              <p style="color:#A7B2E7;" class="d-flex justify-content-start"><b>Ejemplo:</b></p>
               <img src="<?php echo $result1["img_example_exercise"] ?>" class="img-fluid" alt="img">
-              <div class="d-flex justify-content-center mt-2 go">
-                <button class="btn btn-primary" onclick="check()">Verificar</button>
+              <div class="d-flex justify-content-center mt-4 go">
+                <button style="background-color:#8A10F4; color:white;" class="btn" onclick="check()">
+                  <b>Comprobar</b>
+                </button>
               </div>
             </div>
           </div>
@@ -39,7 +43,7 @@
         <textarea style="display:none;" id="idLabel"><?php echo $_GET["idLabel"] ?></textarea>
       </div>
     </div>
-    <div style="background-color:black;" class="col-sm">
+    <div style="background-color:black;" class="bord col-sm">
       <div class="pt-4 ps-1 pe-1" style="display:block;" id="exerciseContent">
         <?php
         $item = "id_language";
@@ -73,8 +77,8 @@
       </div>
       <div style="display:none; margin:25%;" id="resultIncorrect" class="result">
         <div class="alert" role="alert">
-          <b style="color:red;">¡Incorrecto!</b>
-          <button type="button" class="btn btn-link recharge" style="color:#F4ED10;">
+          <h3><b style="color:red;">¡Incorrecto!</b></h3>
+          <button type="button" class="btn btn-link recharge" style="color:#10F44E;">
            <b>¿Intentalo nuevamente?</b> 
           </button>
         </div>
@@ -84,8 +88,8 @@
         <div id="showMessage">
 
         </div>
-        <div class="alert alert-success p-1 mt-4 result" role="alert" style="margin-right:25%;margin-left:25%;">
-          <b>¡Correcto!</b><br>
+        <div class="alert p-1 mt-4 result" role="alert" style="margin-right:25%;margin-left:25%;">
+          <h3><b style="color:#29F410;">¡LO LOGRASTE!</b></h3><br>
           <?php
           $tableExs = "exercises";
           $itemExs2 = "idLabel";
@@ -94,13 +98,14 @@
           if ($result1["number_exercise"] < count($results)) { ?>
             <button idLabel="<?php echo $_GET["idLabel"]; ?>" numberExercise="<?php echo $result1["number_exercise"]; ?>"
               idLanguage="<?php echo $_GET["idLanguage"]; ?>" type="button"
-              class="btn btn-secondary next">Siguiente</button>
+              style="background-color:#10F49B; color:black;" class="btn btn-secondary next">
+              <b>Siguiente</b>&nbsp;<i class="fa-solid fa-arrow-right-to-bracket"></i></button>
           <?php } else { ?>
             <button idLanguage="<?php echo $_GET["idLanguage"]; ?>" type="button" class="btn btn-link openAnother">¿Desea
               aprender algo nuevo?</button>
           <?php } ?>
         </div>
-        <p style="text-align:justify;" class="text-dark"><b>Resultado:</b></p>
+        <p style="text-align:justify; color:#F4108F;" class=""><b>Resultado:</b></p>
         <img src="<?php echo $result1["img_result_exercise"] ?>" class="img-fluid" alt="img"><br>
       </div>
     </div>
